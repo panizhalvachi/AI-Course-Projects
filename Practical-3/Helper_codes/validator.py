5679d39428860d40ef921cc038190214255842e8
panizhalvachi <59363733+panizhalvachi@users.noreply.github.com>
2022-02-22 21:12:47 +0330

    Add files via upload

---
+++ Practical-3/Helper_codes/validator.py
@@ -0,0 +1,29 @@
+import json
+from types import CellType
+import re
+
+
+def extract_python(addr):
+    code = ""
+    with open(addr, 'r') as file:
+        jfile = json.load(file)
+    cells = jfile['cells']
+    collectable_count = 0
+    code_count = 0
+    for cell in cells:
+        if cell['cell_type'] == 'code':
+            code_count += 1
+        if cell['cell_type'] == 'code' and cell['metadata'].get('collectable') == True:
+            code += "".join(list([s for s in cell.get('source') if not re.match("\s*#.+", s)]))
+            collectable_count += 1
+            code+="\n\n"    
+    if collectable_count == 42 and code_count==60:
+        print("your jupyter file will be graded")
+    else:
+        print("your jupyter file won't be graded")
+        print(f"collectable cells' count must be 42 instead found {collectable_count}")
+        print(f"cell count must be 60 instead found {code_count}")
+    return code
+
+if __name__ == "__main__":
+   print( extract_python("../quesions.ipynb"))
